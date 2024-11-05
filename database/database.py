2245d7be52fda26a52224cd51ecf5bb0321b3485
Mingyu Jeong <tomorrow9913@gmail.com>
2024-11-05 17:25:05 +0900

    feat: Now the database url is created using environment variables.

--- database/database.py
+++ database/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DB_URL = "postgresql://postgres:yamong@localhost:5432/yamong"
+username = os.getenv("POSTGRESQL_USERNAME")
+password = os.getenv("POSTGRESQL_PASSWORD")
+host = os.getenv("POSTGRESQL_HOST")
+database = os.getenv("POSTGRESQL_DB")
+port = os.getenv("POSTGRESQL_PORT", "5432")
+
+if not username or not password or not host or not database:
+    raise ValueError("Please set the environment variables for the database")
+
+DB_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
 engine = {
     'project': create_engine(DB_URL),
